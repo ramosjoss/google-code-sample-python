889e36322e7d20d4f194367e30f9d7227d2935c7
Josseline Ramos <ramosjossg@gmail.com>
2021-07-01 12:32:10 +0100

    Submission of Code sample

---
+++ src/video_player.py
@@ -0,0 +1,199 @@
+"""A video player class."""
+
+from .video_library import VideoLibrary
+import random
+
+class VideoPlayer:
+    """A class used to represent a Video Player."""
+    
+    #play_vid = None
+    global vid
+    vid = 0;
+   
+    def __init__(self):
+        self._video_library = VideoLibrary()
+
+    def number_of_videos(self):
+        num_videos = len(self._video_library.get_all_videos())
+        print(f"{num_videos} videos in the library")
+
+    def show_all_videos(self):
+        """Returns all videos."""
+        show_all = self._video_library.get_all_videos()
+
+
+        print("Here's a list of all the available videos:")
+        #new_show_all = sorted(show_all)
+        for i in (show_all):
+            #print (i)
+            print(i._title,i._video_id, i._tags)
+            #new_list.sort()
+            #print (new_list)
+            #print i, max(show_all[i])
+        #print("show_all_videos needs implementation")
+
+    def play_video(self, video_id):
+        """Plays the respective video.
+
+        Args:
+            video_id: The video_id to be played.
+        """
+
+        global vid
+        play_vid = None;
+        if vid == 0:
+            show_all = self._video_library.get_all_videos()
+            for i in show_all:
+                if i._video_id == video_id:
+                    play_vid = i._title;
+                    print("Playing video:", play_vid)
+
+                vid = play_vid
+            return video_id;
+                   
+        else:
+            print("Stopping Video:", vid)
+            show_all = self._video_library.get_all_videos()
+            for i in show_all:
+                if i._video_id == video_id:
+                    play_vid = i._title;
+                    print("Playing video:", play_vid)
+                    vid = play_vid
+            return video_id;
+
+
+        """print("play_video needs implementation")"""
+
+    def stop_video(self):
+        """Stops the current video."""
+        if vid != 0:
+            print("Stopping video:", vid)
+        else:
+            print("Cannot stop video: No video is currently playing")
+        
+        #print("stop_video needs implementation")
+
+    def play_random_video(self):
+        """Plays a random video from the video library."""
+
+        global vid
+        if vid == 0:
+            show_all = self._video_library.get_all_videos()
+            i = random.choice(show_all);
+            play_vid = i._title;
+            print("Playing video:", play_vid)
+            vid = play_vid
+            return vid
+        else:
+            print("Stopping video:", vid)
+            show_all = self._video_library.get_all_videos()
+            x = random.choice(show_all);
+            play_vid = x._title;
+            print("Playing video:", play_vid)
+            vid = play_vid
+            return vid
+
+        #print("play_random_video needs implementation")
+
+    def pause_video(self):
+        """Pauses the current video."""
+
+        print("pause_video needs implementation")
+
+    def continue_video(self):
+        """Resumes playing the current video."""
+
+        print("continue_video needs implementation")
+
+    def show_playing(self):
+        """Displays video currently playing."""
+
+        print("show_playing needs implementation")
+
+    def create_playlist(self, playlist_name):
+        """Creates a playlist with a given name.
+
+        Args:
+            playlist_name: The playlist name.
+        """
+        print("create_playlist needs implementation")
+
+    def add_to_playlist(self, playlist_name, video_id):
+        """Adds a video to a playlist with a given name.
+
+        Args:
+            playlist_name: The playlist name.
+            video_id: The video_id to be added.
+        """
+        print("add_to_playlist needs implementation")
+
+    def show_all_playlists(self):
+        """Display all playlists."""
+
+        print("show_all_playlists needs implementation")
+
+    def show_playlist(self, playlist_name):
+        """Display all videos in a playlist with a given name.
+
+        Args:
+            playlist_name: The playlist name.
+        """
+        print("show_playlist needs implementation")
+
+    def remove_from_playlist(self, playlist_name, video_id):
+        """Removes a video to a playlist with a given name.
+
+        Args:
+            playlist_name: The playlist name.
+            video_id: The video_id to be removed.
+        """
+        print("remove_from_playlist needs implementation")
+
+    def clear_playlist(self, playlist_name):
+        """Removes all videos from a playlist with a given name.
+
+        Args:
+            playlist_name: The playlist name.
+        """
+        print("clears_playlist needs implementation")
+
+    def delete_playlist(self, playlist_name):
+        """Deletes a playlist with a given name.
+
+        Args:
+            playlist_name: The playlist name.
+        """
+        print("deletes_playlist needs implementation")
+
+    def search_videos(self, search_term):
+        """Display all the videos whose titles contain the search_term.
+
+        Args:
+            search_term: The query to be used in search.
+        """
+        print("search_videos needs implementation")
+
+    def search_videos_tag(self, video_tag):
+        """Display all videos whose tags contains the provided tag.
+
+        Args:
+            video_tag: The video tag to be used in search.
+        """
+        print("search_videos_tag needs implementation")
+
+    def flag_video(self, video_id, flag_reason=""):
+        """Mark a video as flagged.
+
+        Args:
+            video_id: The video_id to be flagged.
+            flag_reason: Reason for flagging the video.
+        """
+        print("flag_video needs implementation")
+
+    def allow_video(self, video_id):
+        """Removes a flag from a video.
+
+        Args:
+            video_id: The video_id to be allowed again.
+        """
+        print("allow_video needs implementation")
